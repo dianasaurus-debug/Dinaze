bf5bd8750946fb0ea0dd68320ab447abe30e2b26
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-06-22 23:33:49 +0700

    update some ui for face detection

--- Mobile/lib/view/pages/boarding_screen.dart
+++ Mobile/lib/view/pages/boarding_screen.dart
@@ -1,45 +1,83 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cash_waqf/cubit/face_detection_cubit.dart';
 import 'package:flutter_cash_waqf/cubit/firebase_auth/firebase_auth_cubit.dart';
 import 'package:flutter_cash_waqf/view/widgets/green_button.dart';
 import 'package:flutter_cash_waqf/view/widgets/loading.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BoardingScreen extends StatelessWidget {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return BlocListener<FirebaseAuthCubit, FirebaseAuthState>(
-      listener: (context, state) {
-        if (state is FirebaseAuthFailureState) {
-          AwesomeDialog(
-            context: context,
-            dialogType: DialogType.ERROR,
-            animType: AnimType.BOTTOMSLIDE,
-            dismissOnBackKeyPress: false,
-            dismissOnTouchOutside: false,
-            buttonsBorderRadius: BorderRadius.circular(5),
-            headerAnimationLoop: false,
-            title: "Gagal",
-            desc: state.message,
-            btnCancelOnPress: () {},
-            btnCancelText: "Oke",
-          ).show();
-        }
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<FirebaseAuthCubit, FirebaseAuthState>(
+          listener: (context, state) {
+            if (state is FirebaseAuthFailureState) {
+              AwesomeDialog(
+                context: context,
+                dialogType: DialogType.ERROR,
+                animType: AnimType.BOTTOMSLIDE,
+                dismissOnBackKeyPress: false,
+                dismissOnTouchOutside: false,
+                buttonsBorderRadius: BorderRadius.circular(5),
+                headerAnimationLoop: false,
+                title: "Gagal",
+                desc: state.message,
+                btnCancelOnPress: () {},
+                btnCancelText: "Oke",
+              ).show();
+            }
 
-        if (state is FirebaseAuthNotRegisteredState) {
-          Get.toNamed('/create-password');
-        }
+            if (state is FirebaseAuthNotRegisteredState) {
+              Get.toNamed('/create-password');
+            }
 
-        if (state is FirebaseAuthRegisteredState) {
-          Get.offAllNamed('/main-page');
-        }
-      },
+            if (state is FirebaseAuthRegisteredState) {
+              Get.offAllNamed('/main-page');
+            }
+          },
+        ),
+        BlocListener<FaceDetectionCubit, FaceDetectionState>(
+          listener: (context, state) {
+            if (state is FaceDetectionFailureState) {
+              AwesomeDialog(
+                context: context,
+                dialogType: DialogType.ERROR,
+                animType: AnimType.BOTTOMSLIDE,
+                dismissOnBackKeyPress: false,
+                dismissOnTouchOutside: false,
+                buttonsBorderRadius: BorderRadius.circular(5),
+                headerAnimationLoop: false,
+                title: "Gagal",
+                desc: state.message,
+                btnCancelOnPress: () {},
+                btnCancelText: "Oke",
+              ).show();
+            }
+
+            if (state is FaceDetectionSuccessState) {
+              Get.toNamed('/register');
+            }
+          },
+        ),
+      ],
       child: Scaffold(
+        key: _scaffoldKey,
         body: Center(
-          child: BlocBuilder<FirebaseAuthCubit, FirebaseAuthState>(
-            builder: (context, state) {
-              if (state is FirebaseAuthLoadingState) {
+          child: Builder(
+            builder: (context) {
+              final firebaseAuthState =
+                  context.watch<FirebaseAuthCubit>().state;
+              final faceDetectionState =
+                  context.watch<FaceDetectionCubit>().state;
+
+              if (firebaseAuthState is FirebaseAuthLoadingState ||
+                  faceDetectionState is FaceDetectionLoadingState) {
                 return Loading();
               } else {
                 return ListView(
@@ -53,7 +91,7 @@ class BoardingScreen extends StatelessWidget {
                     Divider(),
                     GreenButton(
                       text: "Registrasi",
-                      onPressedFunction: () => Get.toNamed('/register'),
+                      onPressedFunction: () => _showKycPopup(),
                     ),
                     Divider(),
                     GreenButton(
@@ -74,5 +112,44 @@ class BoardingScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _showKycPopup() => AwesomeDialog(
+        context: _scaffoldKey.currentContext!,
+        dialogType: DialogType.INFO_REVERSED,
+        animType: AnimType.BOTTOMSLIDE,
+        dismissOnBackKeyPress: false,
+        dismissOnTouchOutside: false,
+        buttonsBorderRadius: BorderRadius.circular(5),
+        headerAnimationLoop: false,
+        title: "KYC (Know Your Customer)",
+        desc:
+            "Ambil gambar dengan pose mata kiri tertutup dan mata kanan terbuka",
+        btnOkText: "Oke",
+        btnOkOnPress: () => _detectFace(),
+      ).show();
+
+  _detectFace() async {
+    final image = await ImagePicker().getImage(source: ImageSource.camera);
+
+    if (image != null) {
+      _scaffoldKey.currentContext!
+          .read<FaceDetectionCubit>()
+          .detectFace(image.path);
+    } else {
+      AwesomeDialog(
+        context: _scaffoldKey.currentContext!,
+        dialogType: DialogType.ERROR,
+        animType: AnimType.BOTTOMSLIDE,
+        dismissOnBackKeyPress: false,
+        dismissOnTouchOutside: false,
+        buttonsBorderRadius: BorderRadius.circular(5),
+        headerAnimationLoop: false,
+        title: "Gagal",
+        desc: "Anda belum mengambil gambar",
+        btnCancelText: "Oke",
+        btnCancelOnPress: () {},
+      ).show();
+    }
   }
 }
